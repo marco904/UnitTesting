5fe73e17413aec10697f80c561798f9e0af73cc9
marco904 <marcooccoffer@gmail.com>
2025-02-21 14:44:01 +0100

    modified math_utils.py
    Moved to a class

--- math_utils.py
+++ math_utils.py
@@ -1,20 +1,25 @@
-def add(a: int | float, b: int | float) -> int | float:
-  """
-  Just adds two numbers together.
-  """
-  return a + b
+class MathUtils:
 
-def subtract(a: int | float, b: int | float) -> int | float:
-  """
-  Just subtracts two numbers.
-  """
-  return a - b
-
-def divide(a: int | float, b: int | float) -> float:
-  """
-  Just divides two numbers.
-  """
-  if b == 0:
-    raise ValueError("You cannot divide by zero!")
-  return a / b 
+  @staticmethod
+  def add(a: int | float, b: int | float) -> int | float:
+    """
+    Just adds two numbers together.
+    """
+    return a + b
   
+  @staticmethod
+  def subtract(a: int | float, b: int | float) -> int | float:
+    """
+    Just subtracts two numbers.
+    """
+    return a - b
+
+  @staticmethod
+  def divide(a: int | float, b: int | float) -> float:
+    """
+    Just divides two numbers.
+    """
+    if b == 0:
+      raise ValueError("You cannot divide by zero!")
+    return a / b 
+    
